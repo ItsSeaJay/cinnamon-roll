b5d75c431703828c75141114349fc2be4512b1d0
Unknown <b6010572@my.shu.ac.uk>
2018-02-05 18:37:12 +0000

    Decided on the commands extension

--- source/main.py
+++ source/main.py
@@ -2,6 +2,8 @@ import discord
 import asyncio
 
 client = discord.Client()
+user = discord.User()
+token = 'NDEwMTI4OTE4ODQ3NDIyNDc1.DVop5Q.j3jFteV-w0Z3KeBmdUFGF8C7HYU'
 
 @client.event
 async def on_ready():
@@ -12,7 +14,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!logout'):
+    if message.content.startswith('!exit'):
         await client.logout()
+    elif user.mentioned_in(message):
+        await client.send_message(message.channel, 'Cinnamon Roll')
 
-client.run('NDEwMTI4OTE4ODQ3NDIyNDc1.DVop5Q.j3jFteV-w0Z3KeBmdUFGF8C7HYU')
+client.run(token)
